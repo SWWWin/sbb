b288c5309d12fdc53bde12c68f9c1f381cbf7d3d
SWWWin <bgpd1258@naver.com>
2025-08-14 12:45:50 +0900

    feat: 질문 저장

--- src/main/java/com/mysite/sbb/Question.java
+++ src/main/java/com/mysite/sbb/Question.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -21,4 +22,11 @@ public class Question {
 
 
     private LocalDateTime createTime;
+
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+
+    public void setCreateDate(LocalDateTime now) {
+    }
 }
